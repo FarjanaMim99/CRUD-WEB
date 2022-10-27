2eb5404a8249637a5683c6efe7cf1cba5d2b7323
FarjanaMim99 <109981604+FarjanaMim99@users.noreply.github.com>
2022-10-27 20:07:08 +0600

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
  $fonts = "verdana";
  $bgcolor = "#395144";
+
  $fontcolor = "#FF731D";
 ?>
 
@@ -9,7 +10,7 @@
 <head>
 <br>
 <br>
- <title>PHP OOP CRUD</title>
+ <title>PHP CRUD</title>
  <style>
   body{font-family:<?php echo $fonts;?>}
   .phpcoding{width:900px; margin: 0 auto;
@@ -20,13 +21,17 @@
   .maincontent{min-height:400px;padding:20px;font-size:18px}
   p{margin:0}
  input[type="text"]{width:238px;padding:5px;}
+  
  select{font-size:18px;padding:2px 5px;width:250px;}
  .tblone{width:100%;border:1px solid #fff;margin:20px 0}
+  
  .tblone td{padding:5px 10px;text-align:center;}
  table.tblone tr:nth-child(2n+1){background:#fff;height:30px;}
+  
  table.tblone tr:nth-child(2n){background:#f1f1f1;height:30px;}
  #myform{width:400px;border:1px solid #fff;padding:10px;}
  </style>
+ 
 </head>
 <body>
 
